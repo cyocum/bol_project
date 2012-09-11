35f429a844bae320cc8b16251f18fd195babacbd
Chris Yocum <c.yocum@ed.ac.uk>
2012-09-11 15:31:49 +0100

    Added kmeans and data entry...needs testing

--- src/skmeans/main.cpp
+++ src/skmeans/main.cpp
@@ -3,6 +3,7 @@
 
 #include <mlpack/core.hpp>
 #include <mlpack/core/kernels/cosine_distance.hpp>
+#include <mlpack/core/data/load.hpp>
 #include <mlpack/methods/kmeans/kmeans.hpp>
 
 using namespace std;
@@ -11,10 +12,17 @@ using namespace mlpack;
 using namespace mlpack::kernel;
 using namespace mlpack::metric;
 using namespace mlpack::kmeans;
-
-
+using namespace mlpack::data;
 
 int main(int argc, char **argv) {
+  arma::mat data;
+  data::Load("texts.csv", data, true);
+
+  arma::Col<size_t> output;
+
   KMeans<CosineDistance, RandomPartition, MaxVarianceNewCluster> kmeans;
-  cout << "blah" << endl;
+  kmeans.Cluster(data, 20, output);
+
+  cout << "Cluster Output" << endl;
+  cout << output << endl;
 }
