75f390be2632fe5ba15272adbb7410651e478f51
Chris Yocum <cyocum@gmail.com>
2014-05-25 15:28:57 +0100

    Delete debugging cruft.

--- src/skmeans.jl
+++ src/skmeans.jl
@@ -2,13 +2,7 @@ import Clustering
 import Distance
 
 (data, header) = readcsv("texts.csv", has_header=true)
-tf_idfs = transpose(convert(Array{Float64, 2}, data[:, 2:end]))
-dists = Distance.pairwise(Distance.CosineDist(), tf_idfs)
+tf_idfs = convert(Array{Float64, 2}, data[:, 2:end])
+dists = Distance.pairwise(Distance.CosineDist(), transpose(tf_idfs))
 
-result = Clustering.kmedoids(dists, 20)
-
-clusters = [(result.assignments[idx], name)
-            for (idx, name) in enumerate(data[:, 1])]
-
-sort!(clusters)
-println(clusters)
+@profile result = Clustering.kmedoids(dists, 20)
